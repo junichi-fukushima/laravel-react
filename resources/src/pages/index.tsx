b0d20785c0128dfc75c5919a6fbe4c8f16956ba9
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2022-04-19 18:55:07 +0900

    Reactのバージョンを17に

--- resources/src/pages/index.tsx
+++ resources/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { PrimaryButton } from '../components/atoms/button/PrimaryButton';
 
 const App = () => {
     const title: string = 'hoge';
     return (
+        <>
         <h1>{title}</h1>
+        <PrimaryButton>{title}</PrimaryButton>
+        </>
     )
 }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,13 +4,13 @@ const mix = require('laravel-mix');
 pages配下がルートディレクトリになる
 ・配下のページは基本react-routerなどのReactライブラリを使用する
 ・version()を付与することでハッシュ値を与える(ブラウザキャッシュが残っていることを防ぐ)
-・開発完了時は「npm run prod」を実行するべし
+・開発完了時は「npm run prod」を実行する
 */
 
 mix
-// プロジェクトページ
-.ts('resources/src/pages/project/index.tsx', 'public/js').version()
 // トップページ
 .ts('resources/src/pages/index.tsx', 'public/js').version()
+// プロジェクトページ
+.ts('resources/src/pages/project/index.tsx', 'public/js/project').version()
 // リセットCSSの読み込み
 .postCss('resources/css/app.css', 'public/css')
